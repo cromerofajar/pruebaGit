214fc829b955f6d1b97a357a9a33307de2c89543
David Pérez Piñeiro <davidperez.docente@gmail.com>
2024-09-13 14:08:44 +0200

    inicio

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+<h1>HOLAAA</h1>
+    </header>
+    <main>
+           
+    </main>
+    <footer>
+
+    </footer>
+    <script src="funciones.js"></script>
+</body>
+</html>
