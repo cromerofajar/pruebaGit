2b39bf6fa3835648995e1da04b74beaab3c9bc46
David Pérez Piñeiro <davidperez.docente@gmail.com>
2025-09-16 10:11:18 +0200

    Clonado David profe

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <header>
-<h1>HOLAAAaa</h1>
+        <h1>Primer repositorio clonado con Git</h1>
     </header>
     <main>
-           
+
     </main>
     <footer>
 
     </footer>
     <script src="funciones.js"></script>
 </body>
+
 </html>
