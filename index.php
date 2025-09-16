edcf609e419105c4a707b64259b48a54e9930464
César Romero Fajar <cesarromero.dev@gmail.com>
2025-09-16 12:59:12 +0200

    Cambio Cesar

--- index.php
+++ index.php
@@ -16,19 +16,79 @@
         <p>Lexie Gomez Martinez</p>
         <p>Miguel Ángel Rey</p>
         <p>
-                 _._     _,-'""`-._
-                (,-.`._,'(       |\`-/|
-                    `-.-' \ )-`( , o o)
-                          `-    \`_`"'-
-                         </p>
-                         <p>Juancvl2009</p>
-                         <p>aDFDSF</p>
-        <p> EAEAEEAEAEAEAEA </p>
-        
+            _._ _,-'""`-._
+            (,-.`._,'( |\`-/|
+            `-.-' \ )-`( , o o)
+            `- \`_`"'-
+        </p>
+        <p>Juancvl2009</p>
     </header>
     <main>
-         <p>Hola mundo!</p>
+        <p>Hola mundo!</p>
+        <!DOCTYPE html>
+        <html lang="es">
 
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title></title>
+            <link rel="stylesheet" href="css/style.css">
+        </head>
+
+        <body>
+            <header>
+                <h1>Primer repositorio clonado con Git</h1>
+            </header>
+            <main>
+                <p>Hola mundo!</p>
+                <table>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Apellidos</th>
+                    </tr>
+                    <tr>
+                        <td>David</td>
+                        <td>Pérez Piñeiro</td>
+                    </tr>
+                    <tr>
+                        <td>Pablo</td>
+                        <td>Martinez Alonso</td>
+                    </tr>
+                    <tr>
+                        <td>Lexie</td>
+                        <td>Gomez Martinez</td>
+                    </tr>
+                    <tr>
+                        <td>Miguel</td>
+                        <td>Ángel Rey</td>
+                    </tr>
+                    <tr>
+                        <td>César</td>
+                        <td>Romero Fajar</td>
+                    </tr>
+                    <tr>
+                        <td>Juan</td>
+                        <td>cs2009</td>
+                    </tr>
+                    <tr>
+                        <td>Pablo</td>
+                        <td>Lago EA EA EA EA EA</td>
+                    </tr>
+                </table>
+                <p>
+                    _._ _,-'""`-._
+                    (,-.`._,'( |\`-/|
+                    `-.-' \ )-`( , o o)
+                    `- \`_`"'-
+                </p>
+            </main>
+            <footer>
+
+            </footer>
+            <script src="funciones.js"></script>
+        </body>
+
+        </html>
     </main>
     <footer>
 
